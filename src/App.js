b77dd61e1592cfc736d6db2235c95fa56b68fac0
mdarf <mdarfinji45@gmail.com>
2024-09-11 12:24:34 +0800

    creatinf navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from "./images/logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold bg-red-600 underline">Hello world!</h1>
+    <div>
+      <nav className="bg-deepBlue">
+        <div className="relative w-[1080px] mx-auto flex items-center justify-between">
+          {/* logo */}
+
+          <a href="/" className="cursor-pointer py-7 pr-7 ">
+            <img src={logo} width="125px" height="30px"></img>
+          </a>
+
+          <ul className="flex">
+            <li></li>
+            <li></li>
+
+            <li></li>
+
+            <li></li>
+
+            <li></li>
+
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 }
